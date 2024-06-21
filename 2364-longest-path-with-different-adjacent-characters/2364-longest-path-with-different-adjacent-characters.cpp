37a181f4cbe56a1d129ac3fdedc43f0c5c337fc8
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-06-22 01:08:59 +0530

    Time: 1133 ms (33.97%), Space: 285 MB (12.15%) - LeetHub

---
+++ 2364-longest-path-with-different-adjacent-characters/2364-longest-path-with-different-adjacent-characters.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxlen;
+    int solve(int curr, int parent, unordered_map<int,vector<int>> &adj, string &s)
+    {
+        int longest=0,second_longest=0;
+        for(auto child:adj[curr]){
+            if( child==parent ) continue;
+            int longest_from_child= solve(child, curr, adj, s);
+            if(s[child]==s[curr]) continue;
+
+            if(longest_from_child>second_longest) second_longest=longest_from_child;
+            if(second_longest>longest) swap(second_longest,longest);
+        }
+
+        int niche_hi_milgaya_ans= 1+second_longest+longest;
+        int koi_ek_accha = 1+ max(second_longest , longest);
+        int root_hi_accha= 1;
+        maxlen= max({maxlen, niche_hi_milgaya_ans, koi_ek_accha, root_hi_accha});
+        return max(root_hi_accha, koi_ek_accha);
+    }
+    int longestPath(vector<int>& parent, string s) {
+        maxlen=0;
+        int n=parent.size();
+        unordered_map<int,vector<int>> adj;
+
+        for(int i=1;i<n;i++)
+        {
+            int u=i;
+            int v=parent[i];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        solve(0,-1,adj,s);
+        return maxlen;
+    }
+};
