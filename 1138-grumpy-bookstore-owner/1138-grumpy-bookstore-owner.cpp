1c26d717bdddcb1550bc7d1645e87740fedbe9a8
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-06-21 12:31:25 +0530

    Time: 15 ms (96.59%), Space: 34.4 MB (47.97%) - LeetHub

--- 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.cpp
@@ -1,3 +1,9 @@
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
