a5b75ebfddd0fed18cb5967d8f76547ed34a45fc
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-06-28 20:25:55 +0530

    Time: 193 ms (99.83%), Space: 116.2 MB (97.24%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         vector<int> Degree(n,0);
         for(auto &e:roads)
         {
@@ -8,18 +11,11 @@ public:
             Degree[u]++;
             Degree[v]++;
         }
-        priority_queue<pair<int,int>> pq;
+        sort(Degree.begin(),Degree.end());
+        long long ans=0;
         for(int i=0;i<n;i++)
         {
-            pq.push({Degree[i],i});
-        }
-        long long ans=0;
-        long long val=n;
-        while(!pq.empty())
-        {
-            ans= ans+ val * (long long) pq.top().first;
-            pq.pop();
-            val--;
+            ans= ans+ (long long)Degree[i]* (long long)(i+1);
         }
         return ans;
     }
