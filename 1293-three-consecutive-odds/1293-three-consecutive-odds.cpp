eb34f87a8bea266fe2c23151f48dc5515ed3c167
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-07-02 08:14:37 +0530

    Time: 3 ms (60.84%), Space: 10.5 MB (45.73%) - LeetHub

---
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        if(arr.size()<3) return false;
+        for(int i=2;i<arr.size();i++)
+        {
+            if(arr[i-2]%2==1&&arr[i-1]%2==1&&arr[i]%2==1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
