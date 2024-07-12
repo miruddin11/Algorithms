0c65b4cb9d04d63677c92ded0b36c8399717526a
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-07-12 21:47:33 +0530

    Time: 71 ms (77.64%), Space: 21.5 MB (71.95%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -1,71 +1,31 @@
 class Solution {
 public:
+    string removeSubstr(string &s,string &matchStr)
+    {
+        int i=0;
+        for(int j=0;j<s.size();j++)
+        {
+            s[i]=s[j];
+            i++;
+            if(i>=2&&s[i-2]==matchStr[0]&&s[i-1]==matchStr[1]){
+                i-=2;
+            }
+        }
+        s.erase(s.begin()+i,s.end());
+        return s;
+    }
     int maximumGain(string s, int x, int y) {
+        string maxStr=(x>y)? "ab":"ba";
+        string minStr=(x<y)? "ab":"ba";
         int n=s.size();
-        int points=0;
-        if(y>x)
-        {
-            stack<char> st;
-            for(int i=0;i<n;i++)
-            {
-                char curr=s[i];
-                if(!st.empty()&&st.top()=='b'&&curr=='a'){
-                    points+=y;
-                    st.pop();
-                }
-                else{
-                    st.push(curr);
-                }
-            }
-            string temp;
-            while(!st.empty()){
-                temp.push_back(st.top());
-                st.pop();
-            }
-            for(int i=temp.size()-1;i>=0;i--)
-            {
-                char curr=temp[i];
-                if(!st.empty()&&st.top()=='a'&&curr=='b'){
-                    points+=x;
-                    st.pop();
-                }
-                else{
-                    st.push(curr);
-                }
-            }
-        }
-        else
-        {
-            stack<char> st;
-            for(int i=0;i<n;i++)
-            {
-                char curr=s[i];
-                if(!st.empty()&&st.top()=='a'&&curr=='b'){
-                    points+=x;
-                    st.pop();
-                }
-                else{
-                    st.push(curr);
-                }
-            }
-            string temp;
-            while(!st.empty()){
-                temp.push_back(st.top());
-                st.pop();
-            }
-            for(int i=temp.size()-1;i>=0;i--)
-            {
-                char curr=temp[i];
-                if(!st.empty()&&st.top()=='b'&&curr=='a'){
-                    points+=y;
-                    st.pop();
-                }
-                else{
-                    st.push(curr);
-                }
-            }
-        }
-
-        return points;
+        int ans=0;
+        string temp=removeSubstr(s,maxStr);
+        int L=temp.size();
+        int charsRemoved=(n-L);
+        ans=ans+(charsRemoved/2)*max(x,y);
+        string str=removeSubstr(temp,minStr);
+        charsRemoved=(L-str.size());
+        ans=ans+(charsRemoved/2)*min(x,y);
+        return ans;
     }
 };
