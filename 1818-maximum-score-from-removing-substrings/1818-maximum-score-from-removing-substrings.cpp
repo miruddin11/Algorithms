03a06c25e807c1f8f36a78c18cccbbea9b743add
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-07-12 21:30:56 +0530

    Time: 120 ms (20.73%), Space: 30.5 MB (9.76%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -1,62 +1,36 @@
 class Solution {
 public:
+    string removeSubstr(string &s,string &t)
+    {
+        stack<char> st;
+        for(int i=0;i<s.size();i++)
+        {
+            if(!st.empty()&&st.top()==t[0]&&s[i]==t[1]){
+                st.pop();
+            }
+            else{
+                st.push(s[i]);
+            }
+        }
+        string temp;
+        while(!st.empty()){
+            temp.push_back(st.top());
+            st.pop();
+        }
+        reverse(temp.begin(),temp.end());
+        return temp;
+    }
     int maximumGain(string s, int x, int y) {
+        string maxStr=(x>y)? "ab":"ba";
+        string minStr=(x>y)? "ba":"ab";
         int n=s.size();
-        int points=0;
-        if(y>x)
-        {
-            vector<char> v;
-            for(int i=0;i<n;i++)
-            {
-                char curr=s[i];
-                if(!v.empty()&&v.back()=='b'&&curr=='a'){
-                    points+=y;
-                    v.pop_back();
-                }
-                else{
-                    v.push_back(curr);
-                }
-            }
-            vector<char> v1;
-            for(int i=0;i<v.size();i++)
-            {
-                char curr=v[i];
-                if(!v1.empty()&&v1.back()=='a'&&curr=='b'){
-                    points+=x;
-                    v1.pop_back();
-                }
-                else{
-                    v1.push_back(curr);
-                }
-            }
-        }
-        else
-        {
-            vector<char> v;
-            for(int i=0;i<n;i++)
-            {
-                char curr=s[i];
-                if(!v.empty()&&v.back()=='a'&&curr=='b'){
-                    points+=x;
-                    v.pop_back();
-                }
-                else{
-                    v.push_back(curr);
-                }
-            }
-            vector<char> v1;
-            for(int i=0;i<v.size();i++)
-            {
-                char curr=v[i];
-                if(!v1.empty()&&v1.back()=='b'&&curr=='a'){
-                    points+=y;
-                    v1.pop_back();
-                }
-                else{
-                    v1.push_back(curr);
-                }
-            }
-        }
-        return points;
+        int ans=0;
+        string temp=removeSubstr(s,maxStr);
+        int charsRemoved=(n-temp.size());
+        ans=ans+(charsRemoved/2)*max(x,y);
+        string str=removeSubstr(temp,minStr);
+        charsRemoved=(temp.size()-str.size());
+        ans=ans+(charsRemoved/2)*min(x,y);
+        return ans;
     }
 };
