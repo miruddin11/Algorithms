c6dae66664e910ee60c140ee04f753fcc10e0f1f
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-07-22 11:18:09 +0530

    Time: 28 ms (64.96%), Space: 26.6 MB (60.58%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.cpp
+++ 2502-sort-the-people/2502-sort-the-people.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         map<int,string> mp;
         int n=names.size();
         for(int i=0;i<n;i++)
