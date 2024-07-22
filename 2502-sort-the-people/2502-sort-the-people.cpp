60f7d4f06a21c7fb1751891f478de5b3e4ffaa92
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-07-22 11:26:45 +0530

    Time: 31 ms (50.96%), Space: 29.1 MB (21.9%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.cpp
+++ 2502-sort-the-people/2502-sort-the-people.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
-        cout.tie(nullptr);
-        map<int,string> mp;
         int n=names.size();
-        for(int i=0;i<n;i++)
-        {
-            mp[heights[i]]=names[i];
+        vector< pair<int,string> > mp;
+        for(int i=0;i<n;i++){
+            mp.push_back({heights[i],names[i]});
         }
-        int i=n-1;
-        for(auto &it:mp){
-            names[i]=it.second;
-            i--;
+        sort(mp.rbegin(),mp.rend());
+        vector<string> ans(n);
+        for(int i=0;i<n;i++){
+            ans[i]=mp[i].second;
         }
-        return names;
+        return ans;
     }
 };
