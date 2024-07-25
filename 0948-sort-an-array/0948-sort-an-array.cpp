734c599a9a949878051aa0ab777c849310d5be00
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-07-25 14:50:53 +0530

    Time: 161 ms (55.34%), Space: 98.4 MB (45.68%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,55 +1,20 @@
 class Solution {
 public:
-    void merge(int l,int mid,int r,vector<int> &nums)
-    {
-        vector<int> left,right;
-        int i=0;
-        while(i<mid-l+1){
-            left.push_back(nums[l+i]);
-            i++;
+    vector<int> sortArray(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(auto &x:nums){
+            mp[x]+=1;
         }
+        int mn=*min_element(nums.begin(),nums.end());
+        int mx=*max_element(nums.begin(),nums.end());
         int j=0;
-        while(j<r-mid){
-            right.push_back(nums[mid+1+j]);
-            j++;
-        }
-        i=0,j=0;
-        int k=l;
-        while(i<left.size()&&j<right.size())
-        {
-            if(left[i]<=right[j]){
-                nums[k]=left[i];
-                i++;
-            }
-            else{
-                nums[k]=right[j];
+        for(int i=mn;i<=mx;i++){
+            int f=mp[i];
+            while(f--){
+                nums[j]=i;
                 j++;
             }
-            k++;
         }
-        while(i<left.size()){
-            nums[k]=left[i];
-            i++;
-            k++;
-        }
-        while(j<right.size()){
-            nums[k]=right[j];
-            j++;
-            k++;
-        }
-    }
-    void mergeSort(int l,int r,vector<int> &nums)
-    {
-        if(l<r){
-            int mid=l+(r-l)/2;
-            mergeSort(l,mid,nums);
-            mergeSort(mid+1,r,nums);
-            merge(l,mid,r,nums);
-        }
-    }
-    vector<int> sortArray(vector<int>& nums) {
-        int n=nums.size();
-        mergeSort(0,n-1,nums);
         return nums;
     }
 };
