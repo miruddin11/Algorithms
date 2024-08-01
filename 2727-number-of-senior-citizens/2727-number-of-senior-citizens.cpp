3178d3faed50e321af56dde095358ad48d70f5c0
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-08-01 15:42:45 +0530

    Time: 8 ms (47.01%), Space: 17.6 MB (16.71%) - LeetHub

---
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int cnt=0;
+        for(int i=0;i<details.size();i++)
+        {
+            string s=details[i].substr(11,2);
+            int val=stoi(s);
+            if(val>60) cnt++;
+        }
+        return cnt;
+    }
+};
