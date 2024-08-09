1ed414e4fa32d7abae4eab6c19eb034ce1637100
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-08-09 18:53:12 +0530

    Time: 3 ms (57.14%), Space: 11.9 MB (24.37%) - LeetHub

---
+++ 0870-magic-squares-in-grid/0870-magic-squares-in-grid.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    bool isMagical(vector<vector<int>> &grid,int r,int c)
+    {
+        set<int> st;
+        for(int i=r;i<r+3;i++)
+        {
+            for(int j=c;j<c+3;j++)
+            {
+                if(st.find(grid[i][j])!=st.end()){
+                    return false;
+                }
+                st.insert(grid[i][j]);
+            }
+        }
+        int num=1;
+        for(auto &x:st)
+        {
+            if(x!=num){
+                return false;
+            }
+            num+=1;
+        }
+        int rowSum=grid[r][c]+grid[r][c+1]+grid[r][c+2];
+        for(int i=r;i<r+3;i++)
+        {
+            int sum=0;
+            for(int j=c;j<c+3;j++)
+            {
+                sum+=grid[i][j];
+            }
+            if(sum!=rowSum){
+                return false;
+            }
+        }
+        for(int j=c;j<c+3;j++)
+        {
+            int sum=0;
+            for(int i=r;i<r+3;i++)
+            {
+                sum+=grid[i][j];
+            }
+            if(sum!=rowSum){
+                return false;
+            }
+        }
+        //Diagonal
+        int ldSum=grid[r][c]+grid[r+1][c+1]+grid[r+2][c+2];
+        if(ldSum!=rowSum){
+            return false;
+        }
+        int rdSum=grid[r][c+2]+grid[r+1][c+1]+grid[r+2][c];
+        if(rdSum!=rowSum){
+            return false;
+        }
+        return true;
+    }
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int count=0;
+        int row=grid.size();
+        int col=grid[0].size();
+        for(int i=0;i<=row-3;i++)
+        {
+            for(int j=0;j<=col-3;j++)
+            {
+                if(isMagical(grid,i,j)){
+                    count+=1;
+                }
+            }
+        }
+        return count;
+    }
+};
