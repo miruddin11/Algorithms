ac17a3970efcef2037d64d4665a2cb34583501cd
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-08-15 11:53:43 +0530

    Time: 74 ms (48.46%), Space: 85.9 MB (50.86%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int fiveCnt=0;
+        int TenCnt=0;
+        if(bills[0]!=5){
+            return false;
+        }
+        fiveCnt+=1;
+        for(int i=1;i<bills.size();i++)
+        {
+            if(bills[i]==5){
+                fiveCnt+=1;
+            }
+            else if(bills[i]==10){
+                if(fiveCnt>=1){
+                    fiveCnt-=1;
+                    TenCnt+=1;
+                }
+                else{
+                    return false;
+                }
+            }
+            else if(bills[i]==20){
+                if(TenCnt>=1&&fiveCnt>=1){
+                    TenCnt-=1;
+                    fiveCnt-=1;
+                }
+                else if(fiveCnt>=3){
+                    fiveCnt-=3;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
