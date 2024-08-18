379b336e809ac78e78693820ad4caac0f7899c28
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-08-18 23:40:21 +0530

    Time: 18 ms (73.04%), Space: 12.2 MB (29.84%) - LeetHub

--- 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/2191-minimum-number-of-food-buckets-to-feed-the-hamsters.cpp
+++ 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/2191-minimum-number-of-food-buckets-to-feed-the-hamsters.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     int minimumBuckets(string hamsters) {
         int n=hamsters.size();
-        unordered_map<int,int> mp;
         int ans=0;
         for(int i=0;i<n;i++)
         {
             if(hamsters[i]=='H')
             {
-                if(mp[i-1]>0||mp[i+1]>0) continue;
-                if(i!=n-1&&hamsters[i+1]=='.'){
-                    ans+=1;
-                    mp[i+1]++;
+                if(i-1>=0&&hamsters[i-1]=='B') continue;
+                if(i+1<n&&hamsters[i+1]=='.'){
+                    ans++;
+                    i+=2;
                     continue;
                 }
-                if(i!=0&&hamsters[i-1]=='.'){
-                    ans+=1;
+                if(i-1>=0&&hamsters[i-1]=='.'){
+                    ans++;
                     continue;
                 }
                 return -1;
