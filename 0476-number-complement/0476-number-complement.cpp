b7cd87376040d35ff48ae634060a1460c3f28281
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-08-22 18:13:40 +0530

    Time: 0 ms (100%), Space: 7.5 MB (33.08%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int findComplement(int num) {
-        string bin="";
-        while(num>0){
-            int v=num&1;
-            bin=bin+to_string(v);
-            num>>=1;
-        }
         long long ans=0;
         long long x=1;
-        for(int i=0;i<bin.size();i++){
-            if(bin[i]=='0'){
+        while(num>0){
+            if((num&1)==0){
                 ans+=x;
             }
+            num>>=1;
             x=x*2;
         }
         return ans;
