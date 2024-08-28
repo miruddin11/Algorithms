69ae30bcd2bba9637400c64eccb30c0ac4b82ae8
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-08-28 11:00:54 +0530

    Time: 262 ms (39.1%), Space: 222 MB (14.86%) - LeetHub

--- 2035-count-sub-islands/2035-count-sub-islands.cpp
+++ 2035-count-sub-islands/2035-count-sub-islands.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
+    vector<vector<int>> dir={{0,-1},{0,1},{1,0},{-1,0}};
     bool DFS(int i,int j,vector<vector<int>> &grid1,vector<vector<int>> &grid2)
     {
-        if(i<0||i>=grid2.size()||j<0||j>=grid2[0].size()||grid2[i][j]!=1){
-            return true;
-        }
+        queue<pair<int,int>> q;
+        q.push({i,j});
         grid2[i][j]=-1;
-        bool ans=(grid1[i][j]==1);
-        ans=ans & DFS(i-1,j,grid1,grid2);
-        ans=ans & DFS(i,j+1,grid1,grid2);
-        ans=ans & DFS(i+1,j,grid1,grid2);
-        ans=ans & DFS(i,j-1,grid1,grid2);
+        bool ans=true;
+        while(!q.empty())
+        {
+            int currRow=q.front().first;
+            int currCol=q.front().second;
+            q.pop();
+            if(grid1[currRow][currCol]!=1){
+                ans=false;
+            }
+            for(auto &d:dir)
+            {
+                int row=currRow+d[0];
+                int col=currCol+d[1];
+                if(row<0||row>=grid2.size()||col<0||col>=grid2[0].size()||grid2[row][col]!=1){
+                    continue;
+                }
+                grid2[row][col]=-1;
+                q.push({row,col});
+            }
+        }
         return ans;
     }
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
