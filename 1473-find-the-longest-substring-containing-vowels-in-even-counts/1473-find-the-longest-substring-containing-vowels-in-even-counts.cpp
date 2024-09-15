63cdf6a03bd6372879be9bb0b07736f8b26e7de3
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-09-15 14:47:48 +0530

    Time: 85 ms (34.88%), Space: 19 MB (32.79%) - LeetHub

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -2,37 +2,32 @@ class Solution {
 public:
     int findTheLongestSubstring(string s) {
         int n=s.size();
-        unordered_map<string,int> mp;
-        string currState="00000";
-        mp[currState]=-1;
-        vector<int> vowelCount(5,0); // a e i o u
+        unordered_map<int,int> mp;
+        int mask=0;
+        mp[mask]=-1;
         int maxLen=0;
         for(int i=0;i<n;i++)
         {
             if(s[i]=='a'){
-                vowelCount[0]=(vowelCount[0]^1);
+                mask^=(1<<0);
             }
             else if(s[i]=='e'){
-                vowelCount[1]=(vowelCount[1]^1);
+                mask^=(1<<1);
             }
             else if(s[i]=='i'){
-                vowelCount[2]=(vowelCount[2]^1);
+                mask^=(1<<2);
             }
             else if(s[i]=='o'){
-                vowelCount[3]=(vowelCount[3]^1);
+                mask^=(1<<3);
             }
             else if(s[i]=='u'){
-                vowelCount[4]=(vowelCount[4]^1);
+                mask^=(1<<4);
             }
-            currState="";
-            for(int j=0;j<5;j++){
-                currState+=to_string(vowelCount[j]);
-            }
-            if(mp.find(currState)!=mp.end()){
-                maxLen=max(maxLen,i-mp[currState]);
+            if(mp.find(mask)!=mp.end()){
+                maxLen=max(maxLen,i-mp[mask]);
             }
             else{
-                mp[currState]=i;
+                mp[mask]=i;
             }
         }
         return maxLen;
