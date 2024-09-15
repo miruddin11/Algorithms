16413630d6810f5dc03e692e2a44204098a2b0ee
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-09-15 14:44:18 +0530

    Time: 758 ms (5.12%), Space: 21.2 MB (6.74%) - LeetHub

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -10,19 +10,19 @@ public:
         for(int i=0;i<n;i++)
         {
             if(s[i]=='a'){
-                vowelCount[0]=(vowelCount[0]+1)%2;
+                vowelCount[0]=(vowelCount[0]^1);
             }
             else if(s[i]=='e'){
-                vowelCount[1]=(vowelCount[1]+1)%2;
+                vowelCount[1]=(vowelCount[1]^1);
             }
             else if(s[i]=='i'){
-                vowelCount[2]=(vowelCount[2]+1)%2;
+                vowelCount[2]=(vowelCount[2]^1);
             }
             else if(s[i]=='o'){
-                vowelCount[3]=(vowelCount[3]+1)%2;
+                vowelCount[3]=(vowelCount[3]^1);
             }
             else if(s[i]=='u'){
-                vowelCount[4]=(vowelCount[4]+1)%2;
+                vowelCount[4]=(vowelCount[4]^1);
             }
             currState="";
             for(int j=0;j<5;j++){
