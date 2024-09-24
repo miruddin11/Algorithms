e1637ee877aab4c1f554131e0ff130747c7f02ad
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-09-24 11:45:34 +0530

    Time: 231 ms (84.26%), Space: 160.9 MB (56.99%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,21 +1,52 @@
+class TrieNode{
+    public:
+    TrieNode* children[10];
+};
 class Solution {
 public:
-    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        unordered_set<int> st;
-        for(auto &x:arr1){
-            while(x!=0&&st.find(x)==st.end()){
-                st.insert(x);
-                x/=10;
+    TrieNode* getTrieNode(){
+        TrieNode *node=new TrieNode();
+        for(int i=0;i<10;i++){
+            node->children[i]=NULL;
+        }
+        return node;
+    }
+    void insert(TrieNode *root,int num)
+    {
+        TrieNode* crawl=root;
+        string str=to_string(num);
+        for(auto ch:str){
+            int idx=ch-'0';
+            if(crawl->children[idx]==NULL){
+                crawl->children[idx]=getTrieNode();
+            }
+            crawl=crawl->children[idx];
+        }
+    }
+    int search(TrieNode *root,int num)
+    {
+        TrieNode* crawl=root;
+        string str=to_string(num);
+        int len=0;
+        for(auto ch:str){
+            int idx=ch-'0';
+            if(crawl->children[idx]!=NULL){
+                len++;
+                crawl=crawl->children[idx];
+            }else{
+                break;
             }
         }
+        return len;
+    }
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        TrieNode *root=getTrieNode();
+        for(auto &num:arr1){
+            insert(root,num);
+        }
         int ans=0;
-        for(auto &y:arr2){
-            while(y!=0&&st.find(y)==st.end()){
-                y/=10;
-            }
-            if(y!=0){
-                ans=max(ans,static_cast<int>(log10(y)+1));
-            }
+        for(auto &num:arr2){
+            ans=max(ans,search(root,num));
         }
         return ans;
     }
