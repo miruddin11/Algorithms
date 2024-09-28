c0dc8edbcf8296018d9af438829751039a89fa04
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-09-28 12:31:08 +0530

    Time: 26 ms (10.2%), Space: 22.7 MB (84.26%) - LeetHub

---
+++ 0859-design-circular-deque/0859-design-circular-deque.cpp
@@ -0,0 +1,88 @@
+class MyCircularDeque {
+public:
+    vector<int> deq;
+    int front;
+    int rear;
+    int currCnt;
+    int K;
+    MyCircularDeque(int k) {
+        K=k;
+        currCnt=0;
+        front=0;
+        rear=K-1;
+        deq.resize(K,0);
+    }
+
+    bool insertFront(int value) {
+        if(currCnt==K){
+            return false;
+        }
+        front=(front-1+K)%K;
+        deq[front]=value;
+        currCnt+=1;
+        return true;
+    }
+    
+    bool insertLast(int value) {
+        if(currCnt==K){
+            return false;
+        }
+        rear=(rear+1)%K;
+        deq[rear]=value;
+        currCnt+=1;
+        return true;
+    }
+    
+    bool deleteFront() {
+        if(currCnt==0){
+            return false;
+        }
+        front=(front+1)%K;
+        currCnt-=1;
+        return true;
+    }
+
+    bool deleteLast() {
+        if(currCnt==0){
+            return false;
+        }
+        rear=(rear-1+K)%K;
+        currCnt-=1;
+        return true;
+    }
+    
+    int getFront() {
+        if(currCnt==0){
+            return -1;
+        }
+        return deq[front];
+    }
+    
+    int getRear() {
+        if(currCnt==0){
+            return -1;
+        }
+        return deq[rear];
+    }
+    
+    bool isEmpty() {
+        return (currCnt==0);
+    }
+    
+    bool isFull() {
+        return (currCnt==K);
+    }
+};
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque* obj = new MyCircularDeque(k);
+ * bool param_1 = obj->insertFront(value);
+ * bool param_2 = obj->insertLast(value);
+ * bool param_3 = obj->deleteFront();
+ * bool param_4 = obj->deleteLast();
+ * int param_5 = obj->getFront();
+ * int param_6 = obj->getRear();
+ * bool param_7 = obj->isEmpty();
+ * bool param_8 = obj->isFull();
+ */
