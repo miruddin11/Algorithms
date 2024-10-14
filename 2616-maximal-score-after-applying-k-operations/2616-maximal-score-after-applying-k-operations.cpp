cd4e4b80eadb129de95d854fc5f1cb111f370e52
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-10-14 11:44:48 +0530

    Time: 211 ms (15.71%), Space: 79.7 MB (64.84%) - LeetHub

---
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        long long ans=0;
+        for(auto &x:nums){
+            pq.push(x);
+        }
+        while(k>0)
+        {
+            long long x=pq.top();
+            ans+=x;
+            pq.pop();
+            int v=(x+3-1)/3;
+            pq.push(v);
+            k--;
+        }
+        return ans;
+    }
+};
