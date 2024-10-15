50a69f84c121b7bb536b746b7058f0dc1e2fb572
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-10-15 12:01:45 +0530

    Time: 4 ms (91.1%), Space: 15.8 MB (59.3%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -10,20 +10,20 @@ public:
         return 0;
     }
     int evalRPN(vector<string>& tokens) {
-        stack<string> st;
+        stack<int> st;
         for(auto &s:tokens)
         {
             if(s=="+"||s=="-"||s=="*"||s=="/"){
-                int b=stoi(st.top());
+                int b=st.top();
                 st.pop();
-                int a=stoi(st.top());
+                int a=st.top();
                 st.pop();
-                st.push(to_string(fun(a,b,s)));
+                st.push(fun(a,b,s));
             }
             else{
-                st.push(s);
+                st.push(stoi(s));
             }
         }
-        return stoi(st.top());
+        return st.top();
     }
 };
