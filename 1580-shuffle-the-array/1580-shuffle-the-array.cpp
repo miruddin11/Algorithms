c8fa1c688bb469aa05816e3fd049194757ad1033
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-10-17 00:26:57 +0530

    Time: 4 ms (60.78%), Space: 12.4 MB (60.92%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.cpp
+++ 1580-shuffle-the-array/1580-shuffle-the-array.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector<int> ans;
-        int l=0,r=n;
-        while(l<n&&r<2*n){
-            ans.push_back(nums[l]);
-            ans.push_back(nums[r]);
-            l++;
-            r++;
+        for(int i=0;i<n;i++){
+            ans.push_back(nums[i]);
+            ans.push_back(nums[i+n]);
         }
         return ans;
     }
