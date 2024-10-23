06912c672044f1b5a4a1e484b96d01af853f3cd6
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-10-23 21:58:15 +0530

    Time: 9 ms (99.33%), Space: 317.2 MB (26.5%) - LeetHub

--- 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.cpp
+++ 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.cpp
@@ -14,49 +14,32 @@ public:
     TreeNode* replaceValueInTree(TreeNode* root) {
         queue<TreeNode*> q;
         q.push(root);
-        vector<int> lSum;
-        while(!q.empty())
-        {
-            int sum=0;
-            int size=q.size();
-            while(size--)
-            {
-                TreeNode *curr=q.front();
-                sum+=curr->val;
-                q.pop();
-                if(curr->left!=NULL){
-                    q.push(curr->left);
-                }
-                if(curr->right!=NULL){
-                    q.push(curr->right);
-                }
-            }
-            lSum.push_back(sum);
-        }
-        q.push(root);
-        root->val=0;
-        int l=1;
+        int lSum=root->val;
         while(!q.empty())
         {
             int size=q.size();
+            int nextLSum=0;
             while(size--)
             {
                 TreeNode *curr=q.front();
                 q.pop();
+                curr->val=lSum-curr->val;
                 int siblingSum= curr->left!=NULL?curr->left->val:0;
                 siblingSum+= curr->right!=NULL?curr->right->val:0;
                 if(curr->left!=NULL)
                 {
-                    curr->left->val=lSum[l]-siblingSum;
+                    nextLSum+=curr->left->val;
+                    curr->left->val=siblingSum;
                     q.push(curr->left);
                 }
                 if(curr->right!=NULL)
                 {
-                    curr->right->val=lSum[l]-siblingSum;
+                    nextLSum+=curr->right->val;
+                    curr->right->val=siblingSum;
                     q.push(curr->right);
                 }
             }
-            l++;
+            lSum=nextLSum;
         }       
         return root;
     }
