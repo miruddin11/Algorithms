f682ac71eceec4d2a102b9048d76eefaeda894f2
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-11-09 21:45:21 +0530

    Time: 0 ms (100%), Space: 23.5 MB (69.7%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -12,24 +12,25 @@
 class Solution {
 public:
     int findBottomLeftValue(TreeNode* root) {
+        int ans=root->val;
         queue<TreeNode*> q;
         q.push(root);
-        int leftmost_value;
-
-        while (!q.empty()) {
-            TreeNode* node = q.front();
-            q.pop();
-
-            leftmost_value = node->val;
-
-            if (node->right) {
-                q.push(node->right);
-            }
-            if (node->left) {
-                q.push(node->left);
+        while(!q.empty())
+        {
+            int size=q.size();
+            while(size--)
+            {
+                TreeNode* node=q.front();
+                q.pop();
+                ans=node->val;
+                if(node->right!=NULL){
+                    q.push(node->right);
+                }
+                if(node->left!=NULL){
+                    q.push(node->left);
+                }
             }
         }
-
-        return leftmost_value;
+        return ans;
     }
 };
