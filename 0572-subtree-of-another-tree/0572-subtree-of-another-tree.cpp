0c6e8c52907139cddbfeae1b9856c302412c5b8c
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-11-11 22:44:19 +0530

    Time: 0 ms (100%), Space: 28.1 MB (65.15%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -13,13 +13,13 @@ class Solution {
 public:
     bool isEqual(TreeNode *a,TreeNode *b)
     {
-        if(a==nullptr&&b==nullptr) return true;
-        if(a==nullptr||b==nullptr) return false;
+        if(!a&&!b) return true;
+        if(!a||!b) return false;
         if(a->val!=b->val) return false;
         return isEqual(a->left,b->left)&&isEqual(a->right,b->right);
     }
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(root==nullptr) return false; 
+        if(!root) return false; 
         if(root->val==subRoot->val){
             if(isEqual(root,subRoot)){
                 return true;
