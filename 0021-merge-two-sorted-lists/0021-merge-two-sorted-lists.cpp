a9d2b5ecd2f76465bc5ea7ae3b3b609e9ac956da
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-11-12 22:17:26 +0530

    Time: 0 ms (100%), Space: 19.8 MB (59.02%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,35 +10,16 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL &&list2==NULL) return NULL;
-        if(list1==NULL&&list2!=NULL) return list2;
-        if(list1!=NULL&&list2==NULL) return list1;
-        ListNode *head=list1;
-        if(list1->val > list2->val){
-            head=list2;
-            list2=list2->next;
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(!l1) return l2;
+        if(!l2) return l1;
+        if(l1->val<l2->val){
+            l1->next=mergeTwoLists(l1->next,l2);
+            return l1;
         }
         else{
-            list1=list1->next;
+            l2->next=mergeTwoLists(l1,l2->next);
+            return l2;
         }
-        ListNode *curr=head;
-        while(list1 && list2)
-        {
-            if(list1->val < list2->val)
-            {
-                curr->next=list1;
-                list1=list1->next;
-            }
-            else{
-                curr->next=list2;
-                list2=list2->next;
-            }
-            curr=curr->next;
-        }
-        if(!list1) curr->next=list2;
-        else curr->next=list1;
-
-        return head;
     }
 };
