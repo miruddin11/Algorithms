2073469fb977608528afe039bac2fa9faf87e59c
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-11-23 08:58:07 +0530

    Time: 188 ms (71.67%), Space: 56.3 MB (63.41%) - LeetHub

---
+++ 1972-rotating-the-box/1972-rotating-the-box.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        int m=box.size();
+        int n=box[0].size();
+        for(int r=0;r<m;r++)
+        {
+            int prev=n-1;
+            for(int c=n-1;c>=0;c--)
+            {
+                if(box[r][c]=='#'){
+                    swap(box[r][c],box[r][prev]);
+                    prev-=1;
+                }
+                else if(box[r][c]=='*'){
+                    prev=c-1;
+                }
+            }
+        }
+        vector<vector<char>> ans(n,vector<char>(m));
+        for(int i=0;i<m;i++) 
+        {
+            for(int j=0;j<n;j++)
+            {
+                ans[j][m-1-i]=box[i][j];
+            }
+        }
+        return ans;
+    }
+};
