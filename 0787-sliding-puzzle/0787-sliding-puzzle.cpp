f362b7bc3c6f347a5687e32c64fb0cda147814b9
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-11-25 09:21:27 +0530

    Time: 3 ms (92.37%), Space: 12.4 MB (55.14%) - LeetHub

---
+++ 0787-sliding-puzzle/0787-sliding-puzzle.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int slidingPuzzle(vector<vector<int>>& board) {
+        vector<vector<int>> adj={{1,3},{0,2,4},{1,5},{0,4},{1,3,5},{2,4}};
+        string b;
+        for(auto &row:board)
+        {
+            for(auto &c:row)
+            {
+                b+=to_string(c);
+            }
+        }
+        queue<tuple<int,string,int>> q;
+        q.push({b.find('0'),b,0});
+        unordered_set<string> visit;
+        visit.insert(b);
+        string target="123450";
+        while(!q.empty())
+        {
+            auto [i,b,length]=q.front();
+            q.pop();
+            if (b==target) {
+                return length;
+            }
+            for (auto &j:adj[i]) 
+            {
+                string new_b=b;
+                swap(new_b[i],new_b[j]);
+                if(visit.find(new_b)==visit.end()) {
+                    q.push({j,new_b,length+1});
+                    visit.insert(new_b);
+                }
+            }
+        }
+        return -1;
+
+    }
+};
