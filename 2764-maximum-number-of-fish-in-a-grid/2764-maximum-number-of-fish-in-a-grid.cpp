c285288dbeba7c4dc931b5ac125a4c01113d2b7e
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2025-01-28 20:08:36 +0530

    Time: 23 ms (25.92%), Space: 109.5 MB (21.89%) - LeetHub

---
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int m, n;
+    vector<vector<int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    int bfs(int i, int j, vector<vector<int>>& grid) {
+        queue<pair<int, int>> que;
+        que.push({i, j});
+        int fishCount = grid[i][j];
+        grid[i][j] = 0;
+        while(!que.empty()) {
+            i = que.front().first;
+            j = que.front().second;
+            que.pop();
+            for(auto &dir : directions) {
+                int i_ = dir[0] + i;
+                int j_ = dir[1] + j;
+                if(i_ >= 0 && j_ >= 0 && i_ < m && j_ < n && grid[i_][j_] > 0)
+                {
+                    que.push({i_, j_});
+                    fishCount += grid[i_][j_];
+                    grid[i_][j_] = 0;
+                }
+            }
+        }
+        return fishCount;
+    }
+    int findMaxFish(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        int maxFish = 0;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++)  {
+                if(grid[i][j] > 0) { 
+                    maxFish = max(maxFish, bfs(i, j, grid));
+                }
+            }
+        }
+        return maxFish;
+    }
+};
