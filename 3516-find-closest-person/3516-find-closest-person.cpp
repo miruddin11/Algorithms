33a647ad51904e3741b2f23143f548c9cad0840d
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2025-09-04 19:56:59 +0530

    Time: 0 ms (100%), Space: 8.4 MB (88.8%) - LeetHub

---
+++ 3516-find-closest-person/3516-find-closest-person.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int d1=abs(x-z),d2=abs(y-z);
+        if(d1>d2){
+            return 2;
+        }
+        else if(d2>d1){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+};
