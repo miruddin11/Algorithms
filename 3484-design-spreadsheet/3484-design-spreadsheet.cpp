8292a50320b22b858643f766d85c6ca9a50432c6
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2025-09-19 09:01:55 +0530

    Time: 55 ms (89.1%), Space: 119.7 MB (98.67%) - LeetHub

--- 3484-design-spreadsheet/3484-design-spreadsheet.cpp
+++ 3484-design-spreadsheet/3484-design-spreadsheet.cpp
@@ -1,54 +1,41 @@
 class Spreadsheet {
 public:
-    vector<vector<int>> sheet;
-    Spreadsheet(int rows) {
-        sheet.resize(rows,vector<int>(26,0));
-    }
-    
-    void setCell(string cell, int value) {
-        int col=cell[0]-'A';
-        int row=stoi(cell.substr(1))-1;
-        sheet[row][col]=value;
-    }
-    
-    void resetCell(string cell) {
-        setCell(cell,0);
-    }
+    unordered_map<string, int> sheet;
+    Spreadsheet(int rows) {}
+
+    void setCell(string cell, int value) { sheet[cell] = value; }
+
+    void resetCell(string cell) { sheet.erase(cell); }
     bool containsAlphabet(string s) {
-        for(auto &ch:s){
-            if(isalpha(ch)){
+        for (auto& ch : s) {
+            if (isalpha(ch)) {
                 return true;
             }
         }
         return false;
     }
     int findVal(string x) {
-        if(containsAlphabet(x)){
-            int col=x[0]-'A';
-            int row=stoi(x.substr(1))-1;
-            return sheet[row][col];
-        }
-        else{
+        if (containsAlphabet(x)) {
+            return (sheet.count(x) > 0) ? sheet[x] : 0;
+        } else {
             return stoi(x);
         }
     }
     int getValue(string formula) {
-        string x="",y="";
-        bool yStart=false;
-        for(int i=1;i<formula.size();i++){
-            if(yStart){
-                y+=formula[i];
-            }
-            else if(formula[i]!='+'){
-                x+=formula[i];
-            }
-            else{
-                yStart=true;
+        string x = "", y = "";
+        bool yStart = false;
+        for (int i = 1; i < formula.size(); i++) {
+            if (yStart) {
+                y += formula[i];
+            } else if (formula[i] != '+') {
+                x += formula[i];
+            } else {
+                yStart = true;
             }
         }
-        int val1=findVal(x);
-        int val2=findVal(y);
-        return val1+val2;
+        int val1 = findVal(x);
+        int val2 = findVal(y);
+        return val1 + val2;
     }
 };
 
