321b31c1f4f10aed7058aa3bd7b5e8888d97919e
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2025-10-03 23:52:04 +0530

    Time: 16 ms (96.8%), Space: 17.2 MB (95.06%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
-    typedef pair<int, pair<int, int>> PP;
-    vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    typedef pair<int,pair<int,int>> P;
+    vector<vector<int>> direction={{0,1},{0,-1},{1,0},{-1,0}};
     int trapRainWater(vector<vector<int>>& heightMap) {
-        int m = heightMap.size();
-        int n = heightMap[0].size();
-        priority_queue<PP, vector<PP>, greater<>> boundaryCells;
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        for (int r = 0; r < m; ++r) {
-            for (int c : {0, n - 1}) {
-                boundaryCells.push({heightMap[r][c], {r, c}});
-                visited[r][c] = true;
-            }
+        int n=heightMap.size(),m=heightMap[0].size();
+        priority_queue<P,vector<P>,greater<>> cell;
+        vector<vector<bool>> visited(n,vector<bool>(m,false));
+        //left and right side boundary cell
+        for(int i=0;i<n;i++){
+            cell.push({heightMap[i][0],{i,0}});
+            cell.push({heightMap[i][m-1],{i,m-1}});
+            visited[i][0]=true,visited[i][m-1]=true;
         }
-        for (int c = 0; c < n; ++c) {
-            for (int r : {0, m - 1}) {
-                boundaryCells.push({heightMap[r][c], {r, c}});
-                visited[r][c] = true;
-            }
+        //bottom and lower boundary cell
+        for(int j=0;j<m;j++){
+            cell.push({heightMap[0][j],{0,j}});
+            cell.push({heightMap[n-1][j],{n-1,j}});
+            visited[0][j]=true,visited[n-1][j]=true;
         }
-        int trappedWater = 0;
-        while (!boundaryCells.empty()) {
-            auto [height, cell] = boundaryCells.top();
-            boundaryCells.pop();
-            int i = cell.first;
-            int j = cell.second;
-            for (vector<int>& dir : directions) {
-                int i_ = i + dir[0];
-                int j_ = j + dir[1];
-                if (i_ >= 0 && i_ < m && j_ >= 0 && j_ < n && !visited[i_][j_]) {
-                    trappedWater += max(0, height - heightMap[i_][j_]);
-                    boundaryCells.push({max(height, heightMap[i_][j_]), {i_, j_}});
-                    visited[i_][j_] = true;
+        int trapWater=0;
+        while(!cell.empty()){
+            P t=cell.top();
+            cell.pop();
+            int height=t.first;
+            int i=t.second.first,j=t.second.second;
+            for(auto &vec:direction){
+                int nI=i+vec[0];
+                int nJ=j+vec[1];
+                if(nI>=0&&nI<n&&nJ>=0&&nJ<m&&visited[nI][nJ]==false){
+                    trapWater+=max((height-heightMap[nI][nJ]),0);
+                    cell.push({max(height,heightMap[nI][nJ]),{nI,nJ}});
+                    visited[nI][nJ]=true;
                 }
             }
         }
-        return trappedWater;
+        return trapWater;
     }
 };
