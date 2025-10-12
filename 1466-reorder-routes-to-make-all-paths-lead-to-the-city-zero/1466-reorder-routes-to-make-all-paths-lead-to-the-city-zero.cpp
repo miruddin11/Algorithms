7124817abb3d1e725b2844847054bc67ab2b6201
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2025-10-12 09:42:19 +0530

    Time: 144 ms (37.46%), Space: 123.8 MB (26.92%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int flips;
-    void solve(int u, int parent, vector<bool>& vis,
+    void solve(int u, int parent,
                unordered_map<int, vector<pair<int, int>>>& adj) {
-        vis[u] = true;
         for (auto& [v, f] : adj[u]) {
-            if (!vis[v] && v != parent) {
+            if (v != parent) {
                 if (f == 1) {
                     // getting away from the zero with an existed edge so needs
                     // to flip
                     flips += 1;
                 }
-                solve(v, u, vis, adj);
+                solve(v, u, adj);
             }
         }
     }
@@ -25,8 +24,7 @@ public:
             adj[v].push_back({u, 0});
         }
         flips = 0;
-        vector<bool> vis(n, false);
-        solve(0, -1, vis, adj);
+        solve(0, -1, adj);
         return flips;
     }
 };
