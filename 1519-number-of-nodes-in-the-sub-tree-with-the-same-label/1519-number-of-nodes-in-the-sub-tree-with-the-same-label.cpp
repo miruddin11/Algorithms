2e7734c77d5d96fc9bd3f8f86d2f53aa2ccc81e1
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2025-10-12 17:32:50 +0530

    Time: 262 ms (31.3%), Space: 211.4 MB (22.74%) - LeetHub

---
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> dfs(int u,int parent,unordered_map<int,vector<int>> &adj,string &labels){
+        vector<int> myCount(26,0);
+        int myLabel=labels[u];
+        myCount[myLabel-'a']=1;
+        for(auto &v:adj[u]){
+            if(v!=parent){
+                vector<int> childCount=dfs(v,u,adj,labels);
+                for(int i=0;i<26;i++){
+                    myCount[i]+=childCount[i];
+                }
+            }
+        }
+        ans[u]=myCount[myLabel-'a'];
+        return myCount;
+    }
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
+        ans.resize(n,0);
+        unordered_map<int,vector<int>> adj;
+        for(auto &e:edges){
+            int u=e[0],v=e[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        dfs(0,-1,adj,labels);
+        return ans;
+    }
+};
