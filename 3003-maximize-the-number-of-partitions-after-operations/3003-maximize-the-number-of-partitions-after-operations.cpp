54f24a1836ebfa7765aab8f5987b0e756d3c355b
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2025-10-18 01:31:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3003-maximize-the-number-of-partitions-after-operations/3003-maximize-the-number-of-partitions-after-operations.cpp
+++ 3003-maximize-the-number-of-partitions-after-operations/3003-maximize-the-number-of-partitions-after-operations.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
-    unordered_map<long long, int> mp;
-    string S;
-    int K;
-    int solve(long long i, long long uniqueChars, bool canChange) {
-        long long key = (i << 27) | (uniqueChars << 1) | (canChange);
-        if (mp.count(key)) { 
-            return mp[key];
-        }
-        if (i == S.size()) {
+    unordered_map<long long,int> mp;
+    int solve(int i,long long uniqueChars,bool canChange,string &s,int k){
+        if(i>=s.size()){
             return 0;
         }
-        int characterIndex = S[i] - 'a';
-        int uniqueCharsUpdated = uniqueChars | (1 << characterIndex);
-        int uniqueCharCount = __builtin_popcount(uniqueCharsUpdated);
-        int result;
-        if (uniqueCharCount > K) {
-            result = 1 + solve(i + 1, 1 << characterIndex, canChange);
-        } else {
-            result = solve(i + 1, uniqueCharsUpdated, canChange);
+        long long key=(i<<27)|(uniqueChars<<1)|canChange;
+        if(mp.count(key)){
+            return mp[key];
         }
-        if (canChange) {
-            for (int newCharIndex = 0; newCharIndex < 26; ++newCharIndex) {
-                int newSet = uniqueChars | (1 << newCharIndex);
-                int newUniqueCharCount = __builtin_popcount(newSet);
-                if (newUniqueCharCount > K) {
-                    result = max(result, 1 + solve(i + 1, 1 << newCharIndex, false));
-                } else {
-                    result = max(result, solve(i + 1, newSet, false));
+        int charIdx=s[i]-'a';
+        long long newUniqueChars=uniqueChars|(1LL<<charIdx);
+        int uniqueCharCnt=__builtin_popcountll(newUniqueChars);
+        int ans=0;
+        if(uniqueCharCnt>k){
+            ans=1+solve(i+1,(1LL<<charIdx),canChange,s,k);
+        }
+        else{
+            ans=solve(i+1,newUniqueChars,canChange,s,k);
+        }
+        if(canChange){
+            for(int newCharIdx=0;newCharIdx<26;newCharIdx++){
+                long long newCharSet=uniqueChars|(1LL<<newCharIdx);
+                int newCharCnt=__builtin_popcountll(newCharSet);
+                if(newCharCnt>k){
+                    ans=max(ans,1+solve(i+1,(1LL<<newCharIdx),false,s,k));
+                }
+                else{
+                    ans=max(ans,solve(i+1,newCharSet,false,s,k));
                 }
             }
         }
-        return mp[key] = result;
+        return mp[key]=ans;
     }
     int maxPartitionsAfterOperations(string s, int k) {
-        S = s;
-        K = k;
-        return solve(0, 0, true) + 1;
+        return solve(0,0,true,s,k)+1;
     }
 };
