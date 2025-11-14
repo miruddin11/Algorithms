ba6e00dcb341930cbb0b5b7fa18703312fa48fba
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2025-11-14 21:02:52 +0530

    Time: 36 ms (53.92%), Space: 86.4 MB (89.34%) - LeetHub

---
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        vector<vector<int>> diff(n, vector<int>(n, 0));
+        for (auto& query : queries) {
+            int row1 = query[0];
+            int col1 = query[1];
+            int row2 = query[2];
+            int col2 = query[3];
+            for (int i = row1; i <= row2; i++) {
+                diff[i][col1] += 1;
+                if (col2 + 1 < n)
+                    diff[i][col2 + 1] -= 1;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                diff[i][j] += diff[i][j - 1];
+            }
+        }
+        return diff;
+    }
+};
