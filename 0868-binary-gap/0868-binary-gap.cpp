41940f55631e6ab76753d93b04f938d47fe82b08
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2026-02-22 14:03:39 +0530

    Time: 0 ms (100%), Space: 7.9 MB (94.66%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.cpp
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int binaryGap(int n) {
-        string bin = "";
-        while(n > 0) {
-            bin += to_string(n % 2);
-            n /= 2;
-        }
-        reverse(begin(bin) , end(bin));
         int maxDis = 0;
         int prevIdx = -1;
-        for(int i = 0; i < bin.size(); i++) {
-            if(bin[i] == '0') {
-                continue;
+        int i = 0;
+        while(n) {
+            if(n & 1) {
+                if(prevIdx == -1) {
+                    prevIdx = i;
+                } else {
+                    int dis = i - prevIdx;
+                    maxDis = max(dis , maxDis);
+                    prevIdx = i;
+                }   
             }
-            if(prevIdx == -1) {
-                prevIdx = i;
-            } else {
-                int dis = i - prevIdx;
-                maxDis = max(dis , maxDis);
-                prevIdx = i;
-            }
+            i++;
+            n = n >> 1;
         }
         return maxDis;
     }
