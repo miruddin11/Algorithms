f6f5893dc3ba0bac61f85e71372991f5e78b5845
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2026-04-29 23:05:22 +0530

    Time: 976 ms (5.63%), Space: 39 MB (71.83%) - LeetHub

--- 3225-maximum-score-from-grid-operations/3225-maximum-score-from-grid-operations.cpp
+++ 3225-maximum-score-from-grid-operations/3225-maximum-score-from-grid-operations.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     typedef long long ll;
     int n;
-    ll t[2][101][101];
+    ll dp[2][101][101];
     ll solve(bool prevTaken, int prevHeight, int col, vector<vector<int>>& grid, vector<vector<ll>>& colPrefSum) {
         if(col == n) {
             return 0;
         }
         ll result = 0;
-        if(t[prevTaken][prevHeight][col] != -1) {
-            return t[prevTaken][prevHeight][col];
+        if(dp[prevTaken][prevHeight][col] != -1) {
+            return dp[prevTaken][prevHeight][col];
         } 
         for(int height = 0; height <= n; height++) {
             ll prevColScore = 0;
@@ -24,11 +24,11 @@ public:
             ll currColScoreNotTaken = prevColScore + solve(false, height, col+1, grid, colPrefSum);
             result = max({result, currColScoreTaken, currColScoreNotTaken});
         }
-        return t[prevTaken][prevHeight][col] = result;
+        return dp[prevTaken][prevHeight][col] = result;
     }
     long long maximumScore(vector<vector<int>>& grid) {
         n = grid.size();
-        memset(t, -1, sizeof(t));
+        memset(dp, -1, sizeof(dp));
         vector<vector<ll>> colPrefSum(n + 1, vector<ll>(n + 1, 0));
         for(int col = 1; col <= n; col++) {
             for(int row = 1; row <= n; row++) {
