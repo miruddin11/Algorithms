ca38c5a08b041db201ad1ef1384917b572280c9f
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2026-05-11 23:08:47 +0530

    Time: 5 ms (41.57%), Space: 9.8 MB (34.71%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
     int n, m, N;
-    vector<vector<vector<int>>> dp;
+    vector<vector<int>> dp;
     bool solve(int i, int j, string &s1, string &s2, string &s3) {
         if(i == n && j == m && i + j == N) {
-            return dp[i][j][i + j] = true;
+            return dp[i][j] = true;
         }
         // s3 is formed still either s1 or s2 is left
         if(i + j >= N) {
-            return dp[i][j][i + j] = false;
+            return dp[i][j] = false;
         }
-        if(dp[i][j][i + j] != -1) {
-            return dp[i][j][i + j];
+        if(dp[i][j] != -1) {
+            return dp[i][j];
         }
         bool ans = false;
         if(s1[i] == s3[i + j]) {
             ans = solve(i + 1, j, s1, s2, s3);
         }
         if(ans == true) {
-            return dp[i][j][i + j] = true;
+            return dp[i][j] = true;
         }
         if(s2[j] == s3[i + j]) {
             ans = solve(i, j + 1, s1, s2, s3);
         }
 
-        return dp[i][j][i + j] =  ans;
+        return dp[i][j] =  ans;
     }
     bool isInterleave(string s1, string s2, string s3) {
         n = s1.size(), m = s2.size(), N = s3.size();
-        dp.resize(n + 1, vector<vector<int>>(m + 1, vector<int>(N + 1 , -1)));
         if(n + m != N) {
             return false;
         }
+        dp.resize(n + 1, vector<int>(m + 1, -1));
         return solve(0, 0, s1, s2, s3);
     }
 };
