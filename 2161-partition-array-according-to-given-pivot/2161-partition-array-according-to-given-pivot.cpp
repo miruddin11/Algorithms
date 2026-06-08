3f6f8f3d7a223e3dd83e041b235ad2f08907e315
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2026-06-08 21:13:15 +0530

    Time: 11 ms (30.45%), Space: 127.8 MB (78.53%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -3,15 +3,12 @@ public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         int n = nums.size();
         vector<int> ans(n);
-        int sameCount = 0;
+        int sameCount = count(begin(nums), end(nums), pivot);
         int j = 0;
         for(int i = 0; i < n; i++) {
             if(nums[i] < pivot) {
                 ans[j] = nums[i];
                 j += 1;
-            }
-            if(nums[i] == pivot) {
-                sameCount += 1;
             }
         }
         for(int i = 0; i < sameCount; i++) {
