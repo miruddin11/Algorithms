2a82b5669d0ae3243e46dad98f621bfe09ed57fa
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2026-07-04 09:50:36 +0530

    Time: 91 ms (43.78%), Space: 141.2 MB (32.21%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int mnScore;
-    void dfs(int u,unordered_map<int,vector<pair<int,int>>> &adj,vector<bool> &vis){
-        vis[u]=true;
-        for(auto &[v,d]:adj[u]){
-            mnScore=min(mnScore,d);
-            if(!vis[v]){
-                dfs(v,adj,vis);
+    int ans;
+    void DFS(int u, unordered_map<int, vector<pair<int,int>>> &adj, vector<int> &vis) {
+        vis[u] = 1;
+        for(auto &[v, d] : adj[u]) {
+            ans = min(ans, d);
+            if(vis[v] == 0) {
+                DFS(v, adj, vis);
             }
         }
     }
     int minScore(int n, vector<vector<int>>& roads) {
-        mnScore=INT_MAX;
-        unordered_map<int,vector<pair<int,int>>> adj;
-        for(auto &r:roads){
-            int u=r[0],v=r[1],d=r[2];
-            adj[u].push_back({v,d});
-            adj[v].push_back({u,d});
+        ans = INT_MAX;
+        unordered_map<int, vector<pair<int,int>>> adj;
+        for(auto &r : roads) {
+            int u = r[0], v = r[1], d = r[2];
+            adj[u].push_back({v, d});
+            adj[v].push_back({u, d});
         }
-        vector<bool> vis(n,false);
-        dfs(1,adj,vis);
-        return mnScore;
+        vector<int> vis(n + 1, 0);
+        DFS(1, adj, vis);
+        return ans;
     }
 };
