e23cf7461f2ac5df75ce0f0a84fbaefc0be203b4
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2026-07-31 00:15:02 +0530

    Time: 13 ms (83.97%), Space: 16.6 MB (79.39%) - LeetHub

--- 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.cpp
+++ 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.cpp
@@ -1,82 +1,67 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
-    string smallestPalindrome(string s, long long k) {
-        vector<long long> storage(26, 0);
-        long long remaining = 0;
-        string middle = "";
-
-        for (char ch : s) {
-            storage[ch - 'a']++;
+    long long nCr(int n, int r, int k) {
+        r = min(r, n-r);
+        long long result = 1;
+        for(int i = 1; i <= r; i++) {
+            result = result * (n - r + i)/i; 
+            if(result >= k) 
+                return k;
         }
-
-        for (int i = 0; i < 26; ++i) {
-            if (storage[i] % 2 != 0) {
-                middle = string(1, char(i + 'a'));
-            }
-            storage[i] /= 2;
-            remaining += storage[i];
+        return result;
+    }
+    string smallestPalindrome(string s, int k) {
+        int n = s.length();
+        char mid = ' ';
+        if(n%2 == 1) {
+            mid = s[n/2];
         }
-
-        auto get_perms = [](const vector<long long>& count, long long k) -> long long {
-            __int128 perms = 1;
-            long long N = 0;
-            
-            for (long long c : count) {
-                if (c == 0) continue;
-                N += c;
-                
-                long long r = c;
-                if (r > N - r) r = N - r;
-                
-                __int128 combinations = 1;
-                for (long long i = 1; i <= r; ++i) {
-                    combinations = combinations * (N - i + 1) / i;
-                    if (perms * combinations > k) return k + 1;
-                }
-                
-                perms *= combinations;
-                if (perms > k) return k + 1;
-            }
-            return (long long)perms;
-        };
-
-        long long total = get_perms(storage, k);
-        if (k > total) {
-            return "";
+        vector<int> count(26, 0);
+        for(int i = 0; i < n; i++) {
+            if(n%2 == 1 && i == n/2) continue;
+            count[s[i] - 'a']++;
         }
-
-        string ans = "";
-        
-        for (int step = 0; step < remaining; ++step) {
-            for (int i = 0; i < 26; ++i) {
-                if (storage[i] == 0) {
-                    continue;
-                }
-
-                storage[i]--;
-                long long branch_size = get_perms(storage, k);
-
-                if (k <= branch_size) {
-                    ans.push_back(char(i + 'a'));
-                    break;
-                } else {
-                    k -= branch_size;
-                    storage[i]++;
+        for(int i = 0; i < 26; i++) {
+            count[i] /= 2;
+        }
+        string halfResult = "";
+        int half = n/2;
+        for(int i = 0; i < half; i++) {
+            bool placedCharacter = false; 
+            for(int j = 0; j < 26; j++) {
+                if(count[j] > 0) {
+                    count[j] -= 1;
+                    long long ways = 1;
+                    int letters = 0;
+                    for(int c = 0; c < 26; c++) {
+                        letters += count[c];
+                    }
+                    for(int c = 0; c < 26; c++) {
+                        if(count[c] > 0) {
+                            ways *= nCr(letters, count[c], k);
+                            letters -= count[c];
+                        }
+                        if(ways >= k) {
+                            break;
+                        }
+                    }
+                    if(ways >= k) { 
+                        halfResult.push_back(j + 'a');
+                        placedCharacter = true;
+                        break;
+                    }
+                    k -= ways; 
+                    count[j] += 1;
                 }
             }
+            if(placedCharacter == false)
+                return "";
         }
-
-        string left = ans;
-        string rev_left = left;
-        reverse(rev_left.begin(), rev_left.end());
-
-        return left + middle + rev_left;
+        string rev = halfResult;
+        reverse(begin(rev), end(rev));
+        if(mid != ' ') {
+            halfResult.push_back(mid);
+        }
+        return halfResult + rev;
     }
 };
