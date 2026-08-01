359eb0c7182d333d77f4a141b78eb928ec14592b
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2026-08-02 00:22:09 +0530

    Time: 1 ms (46.17%), Space: 10.3 MB (32.54%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int score(const vector<int>& nums, int l, int r) {
+        if (dp[l][r] != -1) {
+            return dp[l][r];
+        }
+        if (l == r) {
+            return nums[l];
+        }
+        int left = nums[l] - score(nums, l + 1, r);
+        int right = nums[r] - score(nums, l, r - 1);
+        dp[l][r] = max(left, right);
+        return dp[l][r];
+    }
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        dp.assign(n, vector<int>(n, -1));
+        return score(nums, 0, n - 1) >= 0;
+    }
+};
