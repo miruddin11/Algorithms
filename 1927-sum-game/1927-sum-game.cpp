3a8c7f437dce12d6eece9735fdbd652d19d4b234
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2026-08-23 19:45:40 +0530

    Time: 1 ms (77.62%), Space: 14.1 MB (33.57%) - LeetHub

--- 1927-sum-game/1927-sum-game.cpp
+++ 1927-sum-game/1927-sum-game.cpp
@@ -6,10 +6,10 @@ public:
     bool sumGame(string num) {
         int n = num.length();
         double ans = 0.0;
-        for(int i = 0; i < n / 2; ++i) {
+        for (int i = 0; i < n / 2; ++i) {
             ans += getExpectation(num[i]);
         }
-        for(int i = n / 2; i < n; ++i) {
+        for (int i = n / 2; i < n; ++i) {
             ans -= getExpectation(num[i]);
         }
         return ans != 0.0;
